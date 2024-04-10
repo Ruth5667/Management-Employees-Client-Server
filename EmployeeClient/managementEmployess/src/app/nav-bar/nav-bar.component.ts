3caed9f0c9b14d37b791a834a7c25dca5b97a125
Ruth <ruth.in5667@gmail.com>
2024-04-10 15:44:00 +0300

    update css

--- EmployeeClient/managementEmployess/src/app/nav-bar/nav-bar.component.ts
+++ EmployeeClient/managementEmployess/src/app/nav-bar/nav-bar.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import Swal from 'sweetalert2';
-import * as XLSX from 'xlsx';
 import { EmployeeTableComponent } from '../employee-table/employee-table.component';
 import { EmployeeService } from '../services/employee.service';
-
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
@@ -13,9 +11,7 @@ import { EmployeeService } from '../services/employee.service';
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
-  constructor(private router: Router, public _employeeService: EmployeeService) {
-
-  }
+  constructor(private router: Router, public _employeeService: EmployeeService) { }
   logout() {
     sessionStorage.clear();
     this.router.navigate(["/"])
@@ -28,16 +24,5 @@ export class NavBarComponent {
   addEmployee() {
     this.router.navigate(['addEmployee']);
   }
-  exportEmployeesToExcel() {
-    const homeComponent = new EmployeeTableComponent(this._employeeService); // יצירת מופע של הקומפוננטה הביתית
-    homeComponent.exportToExcel(); // קריאה לפונקציה בקומפוננטה הביתית
-    //this.router.navigate(['/']); // ניווט לעמוד הבית
-
-    // this._employeeTableComponent.exportToExcel();
-
-  }
-
-
-
 }
 
